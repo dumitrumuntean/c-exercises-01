45c83e7afbc459c7ae6d2d68e22b5f43e3b23cba
Dumitru Muntean <261824@via.dk>
2018-04-22 17:34:48 +0200

    led is turned on and off continuously with a delay of 500ms

--- HelloWorld/HelloWorld/main.c
+++ HelloWorld/HelloWorld/main.c
@@ -1,10 +1,17 @@
 
 #define F_CPU 10000000L
 #include <avr/io.h>
-
+#include <util/delay.h>
 
 int main(void)
 {
     DDRA = 0xff;            // setting all I/O's in port A as outputs
-    PORTA = ~(4<<PA0);      // turning on led 0 (active low)
+	
+	while(1)
+	{
+		PORTA = ~(4<<PA0);      // turning on led 0 (active low)
+		_delay_ms(500);
+		PORTA = ~(0<<PA0);      // turning on led 0 (active low)
+		_delay_ms(500);
+	}
 }
